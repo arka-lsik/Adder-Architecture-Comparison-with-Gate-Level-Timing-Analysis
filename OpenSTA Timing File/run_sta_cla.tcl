842a5164b1ac10463b4b9b446415fe45cb953ca9
Arkadeep Halder <164157560+arka-lsik@users.noreply.github.com>
2026-05-31 19:40:02 +0530

    timing file for carry look ahead adder(TT corner)

---
+++ OpenSTA Timing File/run_sta_cla.tcl
@@ -0,0 +1,10 @@
+#timing file for carry look ahead adder TT(typical) corner
+
+read_liberty sky130_fd_sc_hd__tt_025C_1v80.lib
+read_verilog cla_netlist.v
+link_design rca
+read_sdc constraints.sdc
+report_timing -path_type full -digits 3
+report_wns
+report_tns
+exit
