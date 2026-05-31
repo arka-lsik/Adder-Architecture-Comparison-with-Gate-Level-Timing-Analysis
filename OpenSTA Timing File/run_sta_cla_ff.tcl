ce41ec5b3a793dda95f2ac869ca28e6b0001336d
Arkadeep Halder <164157560+arka-lsik@users.noreply.github.com>
2026-06-01 00:09:28 +0530

    Add OpenSTA script for carry look ahead adder

---
+++ OpenSTA Timing File/run_sta_cla_ff.tcl
@@ -0,0 +1,10 @@
+#FF corner for carry look ahead adder
+
+read_liberty sky130_fd_sc_hd__ff_n40C_1v95.lib
+read_verilog cla_netlist.v
+link_design cla
+read_sdc constraints.sdc
+report_checks -path_delay max -digits 3
+report_wns
+report_tns
+exit
